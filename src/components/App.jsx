c36ec445fd40203b53538b7de5029a70c0baca17
Oleksii Kravchenko <djdesidos@gmail.com>
2022-10-30 09:33:32 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,17 +27,10 @@ const App = () => {
     setFilter(e.currentTarget.value);
   };
 
-  const getVisibleContacts = () => {
-    //   const normalizedFilter = filter.toLowerCase();
-    //   return contacts.filter(contact =>
-    //     contact.contactName.toLowerCase().includes(normalizedFilter)
-    //   );
-  };
-
   const deleteContact = contactId => {
     setConacts(contacts => [...contacts.filter(({ id }) => id !== contactId)]);
   };
-  const listContacts = getVisibleContacts() ?? contacts;
+  const listContacts = contacts;
   return (
     <Container>
       <Section title="Phonebook">
